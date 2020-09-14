b95919ce41135af31e4f60f838430bcfc183c7cf
nobe4 <haffreinguev@gmail.com>
2020-09-14 08:13:12 +0200

    Improve navigation, display and UX

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* global fetch */
+/* global fetch history */
 'use strict'
 
 const api = 'https://api.datamuse.com/words?{1}={2}'
@@ -15,6 +15,17 @@ input.addEventListener('keydown', function onEvent (event) {
     search(input.value)
   }
 })
+
+window.addEventListener('popstate', function () {
+  window.location.reload()
+})
+
+// Parse existing query parameters
+const query = (new URL(window.location)).searchParams.get('q')
+if (query) {
+  input.value = query
+  search(query)
+}
 
 // Add collapse toggles on titles.
 Array.from(document.getElementsByTagName('h2')).forEach((e) => {
@@ -36,12 +47,24 @@ function populate (type, data) {
     // On click, copy text to the clipboard
     li.addEventListener('click', function () {
       (t => navigator.clipboard.writeText(t))(this.innerText)
+      this.classList.add('copied')
+      setTimeout(_ => {
+        this.classList.remove('copied')
+      }, 1000)
+    })
+    // On doubleclick, search for the term
+    li.addEventListener('dblclick', function () {
+      (t => { input.value = t; search(t) })(this.innerText)
     })
   }
 }
 
 // search runs a query against the api for each type.
 function search (term) {
+  // Push to history only on empty/new query.
+  if (history.state == null || history.state.q !== term) {
+    history.pushState({ q: term }, '', `?q=${term}`)
+  }
   Object.keys(types).forEach(type => {
     var query = api.replace('{1}', types[type]).replace('{2}', term)
     fetch(query)
